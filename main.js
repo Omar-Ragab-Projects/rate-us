5ee02be00c45c9cf77f0439a2e4e3632e2f578df
omar ragb <tryragab@gmail.com>
2024-07-06 07:25:56 +0300

    update

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 const rateNumbers = document.querySelectorAll(".component ul li");
+const submitBtn = document.querySelector(".component input");
+const thankYou = document.querySelector(".thank-you");
+const component = document.querySelector(".component");
+const choosenNumber = document.querySelector(".choosen-number");
+let choosenN;
 rateNumbers.forEach((n) => {
   n.addEventListener("click", (e) => {
     rateNumbers.forEach((rn) => rn.classList.remove("choose"));
     e.target.classList.add("choose");
     document.querySelector("span.error-msg").style.display = "none";
+    choosenN = e.target.textContent;
   });
 });
 let num = 0;
-const submitBtn = document.querySelector(".component input");
-const thankYou = document.querySelector(".thank-you");
-const component = document.querySelector(".component");
+//
 submitBtn.addEventListener("click", (e) => {
   checkChoosen();
   if (num == 1) {
     component.style.display = "none";
     thankYou.classList.add("show");
+    choosenNumber.textContent = choosenN;
   } else {
     console.log("error");
     document.querySelector("span.error-msg").style.display = "block";
